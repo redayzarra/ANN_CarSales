f526df34aee572ce784c86710f2546504152dbe9
ReDay Zarra <redayzarra@gmail.com>
2023-01-16 11:30:53 -0500

    Importing the dataset

--- Car_Sales_Regression.py
+++ Car_Sales_Regression.py
@@ -1,4 +1,9 @@
 """
-This project utilizes a neural network to perform a regression task, which means
-the model will predict a value based on the 
+Car Sales - Regression ANN
+
+This project utilizes an artificial neural network to perform a regression task, 
+which means the model will predict a value based on the features. In this case, 
+the model will have eight features to predict the purchasing amount for each 
+customer.This project showcases a step-by-step implementation of the model as 
+well as in-depth notes to customize the model further for higher accuracy.
 """
